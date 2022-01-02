156f6fff76259392f1bcdfa9560271da6f2d8e0d
ckyle121 <carolinekyle21@gmail.com>
2022-01-02 09:03:56 -0600

    create array of quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,45 @@
-// function to start timer 
+// ARRAY of QUIZ QUESTIONS
+var questions = [
+    // q 1
+    {
+        question:"Which of the follow is not a JavaScript Data Type?",
+        choices:["a. Number", "b. Undefined", "c. Boolean", "d. Float"],
+        answer:"d. Float"
+    },
+    // q 2 
+    {
+        question:"Inside which HTML element do we put the JavaScript?",
+        choices:["a. <script>", "b. <head>", "c. <javascript>", "d. <style>"],
+        answer:"a. <script>"
+    },
+    // q 3
+    {
+        question:"JavaScript ignores...",
+        choices:["a. Spaces", "b. Tabs", "c. Newlines", "d. All of the above"],
+        answer:"d. All of the above"
+    },
+    // q 4 
+    {
+        question:"Which symbol is used for comments in JavaScript?",
+        choices:["a. \\", "b. #", "c. //", "d. \* */"],
+        answer:"c. //"
+    },
+    // q 5
+    {
+        question:"Arrays in JavaScript can be used to store _____.",
+        choices: ["a. Numbers and Strings", "b. Other Arrays", "c. Objects", "d. All of the above"],
+        answer: "d. All of the above"
+    }
+];
+
+
+
+
+// function to start timer/quiz
+
+var startQuiz = function(){
+
+};
 
 // loop through series of questions 
 
