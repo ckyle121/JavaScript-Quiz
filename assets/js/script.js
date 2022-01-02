2aff34f87ac541089a2349188fc8492692f20493
ckyle121 <carolinekyle21@gmail.com>
2022-01-02 08:41:28 -0600

    initial file and html css set up

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// function to start timer 
+
+// loop through series of questions 
+
+// subtract time bc incorrect question
+
+// quiz ends when all questions are answered or timer is over
+
+// save initials and high score to local storage 
